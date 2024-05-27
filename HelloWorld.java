4ae0aef77b842d7c965e0221416cbff3904ee2e3
Jocyline Magero <jmagero54@gmail.com>
2024-05-27 12:03:52 -0500

    add change

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,6 @@ public class HelloWorld{
         for (int i = 0; i < args.length; i++) {
             System.out.println(i);
         }
-        System.out.println("My change");
+        System.out.println("New change");
     }
 }
